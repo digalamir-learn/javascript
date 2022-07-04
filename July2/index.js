2fb4f3a606c34bf6b952239e527def8b7db195dc
digalamir-learn <106925676+digalamir-learn@users.noreply.github.com>
2022-07-04 09:52:42 +0545

    new javascript update

--- July2/index.js
+++ July2/index.js
@@ -2,13 +2,10 @@ let date;
 let url = "https://api.nasa.gov/planetary/apod?"
 
 date = document.getElementById("sngDate").value;
-let data = {
-    "date" : date,
-    "api_key" : "DEMO_KEY"
-};
+
 
 const submitButton = document.getElementById('sngBtn');
-let newurl = url.searchParams.append()
-submitButton.onclick = fucntion() {
-    let response = await fetch('')
+let newurl = url+"date="+date+"?"+"api_key="+"DEMO_KEY";
+submitButton.onclick = fucntion() async {
+    let response = await fetch('newurl')
 }
